f0f486bf48bdbcb9db6afc83ae7bee997d1fd777
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-04-21 14:15:06 +0200

    no internet no deps

--- src/Magpie.Core/ImGuiFontsCacheManager.h
+++ src/Magpie.Core/ImGuiFontsCacheManager.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <imgui.h>
-#include <parallel_hashmap/phmap.h>
+//#include ?<imgui.h>
+//#include <parallel_hashmap/phmap.h>
 
 namespace Magpie {
 
--- src/Magpie.Core/include/EffectCompiler.h
+++ src/Magpie.Core/include/EffectCompiler.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <parallel_hashmap/phmap.h>
+//#include <parallel_hashmap/phmap.h>
 
 namespace Magpie {
 
