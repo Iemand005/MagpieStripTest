1a44d641141fc62f182832f2dbc4177b1cafdf1a
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-04-21 11:40:44 +0200

    empty out

--- src/Magpie/App.h
+++ src/Magpie/App.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "App.g.h"
-#include "AppSettings.h"
 #include "Event.h"
-#include <winrt/Windows.Globalization.NumberFormatting.h>
 #include <winrt/Windows.UI.Xaml.Hosting.h>
 
 namespace Magpie {
@@ -15,101 +13,80 @@ struct RootPage;
 
 class App : public App_base<App, Markup::IXamlMetadataProvider> {
 public:
-	static App& Get();
+static App& Get();
 
-	App();
-	App(const App&) = delete;
-	App(App&&) = delete;
+App();
+App(const App&) = delete;
+App(App&&) = delete;
 
-	bool Initialize(const wchar_t* arguments);
+bool Initialize(const wchar_t* arguments);
 
-	int Run();
+int Run();
 
-	const DispatcherQueue& Dispatcher() const noexcept {
-		return _dispatcher;
-	}
+const DispatcherQueue& Dispatcher() const noexcept {
+return _dispatcher;
+}
 
-	void ShowMainWindow() noexcept;
+void ShowMainWindow() noexcept;
 
-	void Quit();
+void Quit();
 
-	void Restart(bool asElevated = false, const wchar_t* arguments = nullptr) noexcept;
+const com_ptr<RootPage>& RootPage() const noexcept;
 
-	const com_ptr<RootPage>& RootPage() const noexcept;
+const ::Magpie::MainWindow& MainWindow() const noexcept {
+return *_mainWindow;
+}
 
-	const ::Magpie::MainWindow& MainWindow() const noexcept {
-		return *_mainWindow;
-	}
+::Magpie::MainWindow& MainWindow() noexcept {
+return *_mainWindow;
+}
 
-	::Magpie::MainWindow& MainWindow() noexcept {
-		return *_mainWindow;
-	}
+bool IsLightTheme() const noexcept {
+return _isLightTheme;
+}
 
-	bool IsLightTheme() const noexcept {
-		return _isLightTheme.load(std::memory_order_relaxed);
-	}
-
-	static Windows::Globalization::NumberFormatting::INumberFormatter2 DoubleFormatter();
-
-	::Magpie::MultithreadEvent<bool> ThemeChanged;
+::Magpie::MultithreadEvent<bool> ThemeChanged;
 
 private:
-	void _Uninitialize();
+void _Uninitialize();
 
-	bool _CheckSingleInstance() noexcept;
+void _UpdateTheme();
 
-	void _AppSettings_ThemeChanged(::Magpie::AppTheme theme);
+Hosting::WindowsXamlManager _windowsXamlManager{ nullptr };
 
-	void _UpdateColorValuesChangedRevoker();
+std::unique_ptr<::Magpie::MainWindow> _mainWindow;
 
-	void _UpdateTheme();
+DispatcherQueue _dispatcher{ nullptr };
 
-	void _ReleaseMutexes() noexcept;
+bool _isLightTheme = true;
 
-	wil::unique_mutex_nothrow _hSingleInstanceMutex;
-	// 以管理员身份运行时持有此锁
-	wil::unique_mutex_nothrow _hElevatedMutex;
-
-	Hosting::WindowsXamlManager _windowsXamlManager{ nullptr };
-
-	std::unique_ptr<::Magpie::MainWindow> _mainWindow;
-
-	DispatcherQueue _dispatcher{ nullptr };
-
-	::Magpie::Event<::Magpie::AppTheme>::EventRevoker _themeChangedRevoker;
-	Windows::UI::ViewManagement::UISettings _uiSettings;
-	Windows::UI::ViewManagement::UISettings::ColorValuesChanged_revoker _colorValuesChangedRevoker;
-	std::atomic<bool> _isLightTheme = true;
-
-	::Magpie::Event<bool>::EventRevoker _isShowNotifyIconChangedRevoker;
-
-	////////////////////////////////////////////////////
-	// 
-	// IXamlMetadataProvider 相关
-	// 
-	/////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+// 
+// IXamlMetadataProvider related
+// 
+/////////////////////////////////////////////////////
 public:
-	Markup::IXamlType GetXamlType(Interop::TypeName const& type) {
-		return _AppProvider()->GetXamlType(type);
-	}
+Markup::IXamlType GetXamlType(Interop::TypeName const& type) {
+return _AppProvider()->GetXamlType(type);
+}
 
-	Markup::IXamlType GetXamlType(hstring const& fullName) {
-		return _AppProvider()->GetXamlType(fullName);
-	}
+Markup::IXamlType GetXamlType(hstring const& fullName) {
+return _AppProvider()->GetXamlType(fullName);
+}
 
-	com_array<Markup::XmlnsDefinition> GetXmlnsDefinitions() {
-		return _AppProvider()->GetXmlnsDefinitions();
-	}
+com_array<Markup::XmlnsDefinition> GetXmlnsDefinitions() {
+return _AppProvider()->GetXmlnsDefinitions();
+}
 
 private:
-	com_ptr<XamlMetaDataProvider> _AppProvider() {
-		if (!_appProvider) {
-			_appProvider = make_self<XamlMetaDataProvider>();
-		}
-		return _appProvider;
-	}
+com_ptr<XamlMetaDataProvider> _AppProvider() {
+if (!_appProvider) {
+_appProvider = make_self<XamlMetaDataProvider>();
+}
+return _appProvider;
+}
 
-	com_ptr<XamlMetaDataProvider> _appProvider;
+com_ptr<XamlMetaDataProvider> _appProvider;
 };
 
 }
